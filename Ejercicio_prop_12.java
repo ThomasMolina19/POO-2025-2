03b3e47856f8488765bb38a3800ea26cd36d86c2
Thomas Molina Molina <99705383+ThomasMolina19@users.noreply.github.com>
2025-09-08 22:15:53 -0500

    Add package declaration to Ejercicio_prop_12.java

---
+++ Ejercicio_prop_12.java
@@ -0,0 +1,15 @@
+package POO-2025-2.Taller_1;
+
+public class Ejercicio_prop_12 {
+    public static void main(String[] args) {
+        double Horas,CostoPorHora,PorcentajeRetencion,SalarioBruto,SalarioNeto;
+        Horas = 40;
+        CostoPorHora = 5000;
+        SalarioBruto = Horas*CostoPorHora;
+        PorcentajeRetencion = (0.125*SalarioBruto);
+        SalarioNeto = SalarioBruto-PorcentajeRetencion;
+        System.out.println("Salario bruto: "+SalarioBruto);
+        System.out.println("Retencion en la fuente: "+PorcentajeRetencion);
+        System.out.println("Salario neto: "+SalarioNeto );
+    }
+}
