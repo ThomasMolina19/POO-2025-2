702e6a568dac006fe919bc2ae0bf2b8a35f6315f
Thomas Molina Molina <99705383+ThomasMolina19@users.noreply.github.com>
2025-09-08 22:16:29 -0500

    Add Ejercicio_prop_14.java to calculate powers

---
+++ Ejercicio_prop_14.java
@@ -0,0 +1,13 @@
+package POO-2025-2.Taller_1;
+
+public class Ejercicio_prop_14 {
+    public static void main(String[] args) {
+        double Numero,CuadradoNum, CuboNum;
+        Numero = 4;
+        CuadradoNum = Math.pow(Numero, 2);
+        CuboNum = Math.pow(Numero,3);
+        System.out.println("Numero original: "+Numero);
+        System.out.println("Numero al cuadrado: "+CuadradoNum); 
+        System.out.println("Numero al cubo: "+CuboNum);
+    }
+}
