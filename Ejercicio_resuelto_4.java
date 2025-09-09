a928c622282944e42aa8bbd6ddc7999538b32d2a
Thomas Molina Molina <99705383+ThomasMolina19@users.noreply.github.com>
2025-09-08 22:08:23 -0500

    Add Ejercicio_resuelto_4.java to calculate ages

---
+++ Ejercicio_resuelto_4.java
@@ -0,0 +1,16 @@
+package POO-2025-2.Taller_1;
+
+public class Ejercicio_resuelto_4 {
+    public static void main(String[] args) {
+        double edjuan = 9;
+        double edalber,edana,edmama;
+        edalber = (2*edjuan)/3;
+        edana = (4*edjuan)/3;
+        edmama = edjuan+edana+edalber;
+        System.out.println("Las edades son:");
+        System.out.println("Alberto: "+edalber);
+        System.out.println("Juan: "+edjuan);
+        System.out.println("Ana: "+edana);
+        System.out.println("Mamá: "+edmama);
+    }
+}
