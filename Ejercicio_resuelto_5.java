9e32d4a615582422d029adcaf4f8bb18da0b37a7
Thomas Molina Molina <99705383+ThomasMolina19@users.noreply.github.com>
2025-09-08 22:14:55 -0500

    Add Ejercicio_resuelto_5.java with main method

---
+++ Ejercicio_resuelto_5.java
@@ -0,0 +1,13 @@
+package POO-2025-2.Taller_1;
+
+public class Ejercicio_resuelto_5 {
+    public static void main(String[] args) {
+        double suma=0; double x,y;
+        x=20;
+        y=40;
+        suma+=x;
+        x+=Math.pow(y,2);
+        suma+=(x/y);
+        System.out.println("El valor de la suma es: "+suma);
+    }
+}
