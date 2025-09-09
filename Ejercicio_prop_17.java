4f63e5d6dc442b34347e85bd3ba4d411317023b7
Thomas Molina Molina <99705383+ThomasMolina19@users.noreply.github.com>
2025-09-08 22:18:10 -0500

    Create Ejercicio_prop_17.java to calculate circle properties

---
+++ Ejercicio_prop_17.java
@@ -0,0 +1,13 @@
+package POO-2025-2.Taller_1;
+
+public class Ejercicio_prop_17 {
+    public static void main(String[] args) {
+        double radio, area, longCircunferencia;
+        radio = 3;
+        area = Math.PI*Math.pow(radio,2);
+        longCircunferencia = Math.PI*2*radio;
+        System.out.println("Radio del circulo: "+radio);
+        System.out.println("Area del circulo: "+area);
+        System.out.println("Longuitud de la circunferencia: "+longCircunferencia);
+    }
+}
